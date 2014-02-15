add567f24b7d2e0959b6334b81f1692a0967d68b
TheRenderJunkie <therenderjunkie@gmail.com>
2014-02-15 01:59:39 -0600

    Update mdwrapper.py
    New HTML Layout.  Menu links will need updating as pages are added.

--- tools/mdwrapper.py
+++ tools/mdwrapper.py
@@ -10,31 +10,62 @@ else:
 		text = f.read()
 
 sys.stdout.write("""
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>%s</title>
-<link rel="stylesheet" href="/style.css">
-</head>
 <body>
-<table id="wrapper">
-<tr id="header">
-  <th id="logo">
-    <a href="/"><img alt="Chunky logo" src="/logo.png"></a>
-  </th>
-  <th id="title">
-    <h1>Chunky</h1>
-    <h2>Minecraft world renderer</h2>
-  </th>
-</tr>
-<tr id="content">
-  <td id="menu"></td>
-  <td id="article">
+	<div id="wrapper">
+		<div id="header">
+	    <div id="logo">
+	    	<a href="index.html"><img src="images/logo.png" /></a>
+	    </div>
+	    <div id="title">
+	    	<h1 class="title">Chunky</h1>
+	        <h2 class="subtitle">Minecraft World Renderer</h2>
+		</div>
+	       </div>
+	<div id="content">
+		<div id="menu">
+	      <ul class="menu-text">	
+	        <li><a href="index.html">home</a></li>
+	        <li><a href="getting started.html">getting started</a></li>
+	            <ul class="menu-text-indented">
+	                <li><a href="install.html">installation</a></li>
+	                <li><a href="#">tutorials</a></li>
+	            </ul>
+	        <li><a href="#">faqs</a></li>
+	        <li><a href="#">user guides</a></li>
+	            <ul class="menu-text-indented">
+	            	<li><a href="path_tracing.html">path tracing</a>
+	                <li><a href="headless.html">headless rendering</a></li>
+	                <li><a href="scene_format.html">scene discription format</a></li>
+	            </ul>
+	        <li><a href="#">control guides</a></li>
+	            <ul class="menu-text-indented">
+	                <li><a href="#">2d map preview</a></li>
+	                <li><a href="#">render controls</a></li>
+	                <li><a href="render_preview.html">render preview window</a></li>
+	            </ul>
+	        <li><a href="skymaps.html">skymaps</a></li>
+	      </ul>
+	      <hr class="hr-padded"/>
+	      <ul class="menu-text">
+	        <li><a href="http://github.com/llbit/chunky">github page</a></li>
+	        <li><a href="http://github.com/llbit/chunky/issues">issue tracker</a></li>
+	        <li><a href="http://www.reddit.com/r/chunky">reddit community</a></li>
+	      </ul>
+	      <hr class="hr-padded"/>
+	      <ul class="menu-text">
+	        <li><a href="galleries.html">galleries</a></li>
+	      </ul>
+	      <hr class="hr-padded"/>
+	      <ul class="menu-text">
+	      	<li><a href="credits.html">credits</a></li>
+	      </ul>
+	    </div><!--End Menu Div-->
+	    <!--Content goes here -->
 """ % text.split('\n', 1)[0])
 sys.stdout.write(markdown.markdown(text).encode('utf-8'))
 sys.stdout.write("""
-  </td>
-</tr>
-</table></body></html>""")
+       	</div>
+</div>
+</body>
+</html>""")
 sys.stdout.flush()
