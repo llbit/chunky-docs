9b72e69f1bb604faaed5db7bcd321606131d6434
Jesper Öqvist <jesper@llbit.se>
2014-02-24 23:27:27 +0100

    Added download page

---
+++ docs/download.md
@@ -0,0 +1,13 @@
+Download Links
+==============
+
+Download links for [version @VERSION@][1]:
+
+* [Windows Installer][2]
+* [Cross-platform binaries (Mac, Win, Linux)][3]
+* [Launcher only (Mac, Win, Linux)][4]
+
+[1]: release/@VERSION@/release_notes.html
+[2]: @EXE_DL_LINK@
+[3]: @ZIP_DL_LINK@
+[4]: http://chunkyupdate.llbit.se/ChunkyLauncher.jar
--- tools/mdwrapper.py
+++ tools/mdwrapper.py
@@ -34,49 +34,50 @@ sys.stdout.write("""
 		<tr id="content">
 			<td id="menu">
 				<ul class="menu-text">
-			<li><a href="index.html">Home</a></li>
-			<li><a href="getting_started.html">Getting Started</a></li>
-				<ul class="menu-text-indented">
-					<li><a href="install.html">Installation</a></li>
-					<li><a href="tips_tutorials.html">Tips / Tutorials</a></li>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="download.html" class="download">Download</a></li>
+					<li><a href="getting_started.html">Getting Started</a></li>
+						<ul class="menu-text-indented">
+							<li><a href="install.html">Installation</a></li>
+							<li><a href="tips_tutorials.html">Tips / Tutorials</a></li>
+						</ul>
+					<li><a href="faqs.html">FAQs</a></li>
+					<li>User Guides</li>
+						<ul class="menu-text-indented">
+							<li><a href="path_tracing.html">Path Tracing</a>
+							</li><li><a href="headless.html">Headless Rendering</a></li>
+							<li><a href="scene_format.html">Scene Discription Format</a></li>
+						</ul>
+					<li>UI Guides</li>
+						<ul class="menu-text-indented">
+							<li><a href="2d_map_view.html">2D Map View</a></li>
+							<li><a href="render_controls.html">Render Controls</a></li>
+							<li><a href="render_preview.html">Render Preview Window</a></li>
+						</ul>
 				</ul>
-			<li><a href="faqs.html">FAQs</a></li>
-			<li>User Guides</li>
-				<ul class="menu-text-indented">
-				<li><a href="path_tracing.html">Path Tracing</a>
-				</li><li><a href="headless.html">Headless Rendering</a></li>
-				<li><a href="scene_format.html">Scene Discription Format</a></li>
+				<hr class="hr-padded">
+				<ul class="menu-text">
+					<li><a href="http://github.com/llbit/chunky">GitHub Page</a></li>
+					<li><a href="http://github.com/llbit/chunky/issues">Issue Tracker</a></li>
+					<li><a href="http://www.reddit.com/r/chunky">Reddit Community</a></li>
 				</ul>
-			<li>UI Guides</li>
-				<ul class="menu-text-indented">
-				<li><a href="2d_map_view.html">2D Map View</a></li>
-				<li><a href="render_controls.html">Render Controls</a></li>
-				<li><a href="render_preview.html">Render Preview Window</a></li>
+				<hr class="hr-padded">
+				<ul class="menu-text">
+					<li><a href="galleries.html">Galleries</a></li>
+					<li><a href="skymaps.html">Skymaps</a></li>
 				</ul>
-			</ul>
-			<hr class="hr-padded">
-			<ul class="menu-text">
-				<li><a href="http://github.com/llbit/chunky">GitHub Page</a></li>
-				<li><a href="http://github.com/llbit/chunky/issues">Issue Tracker</a></li>
-				<li><a href="http://www.reddit.com/r/chunky">Reddit Community</a></li>
-			</ul>
-			<hr class="hr-padded">
-			<ul class="menu-text">
-				<li><a href="galleries.html">Galleries</a></li>
-				<li><a href="skymaps.html">Skymaps</a></li>
-			</ul>
-			<hr class="hr-padded">
-			<ul class="menu-text">
-				<li><a href="contributing.html">Contributing</a></li>
-				<li><a href="credits.html">Credits</a></li>
-			</ul>
-		</td><!--End Menu Div-->
-		<!--Content goes here -->
-		<td id="article">
+				<hr class="hr-padded">
+				<ul class="menu-text">
+					<li><a href="contributing.html">Contributing</a></li>
+					<li><a href="credits.html">Credits</a></li>
+				</ul>
+			</td><!--End Menu Div-->
+			<!--Content goes here -->
+			<td id="article">
 """ % title)
 sys.stdout.write(markdown.markdown(text, ['extra']).encode('utf-8'))
 sys.stdout.write("""
-		</td>
+			</td>
 		</tr>
 	</table>
 </body>
