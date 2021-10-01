40484f86d2e63afebd6b4597ecb1ab5a0792e3d6
Jack Peters <jackjt8@gmail.com>
2021-10-01 22:07:01 +0100

    revert custom java_path

--- gradlew.bat
+++ gradlew.bat
@@ -33,7 +33,7 @@ goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=D:/Games/MinecraftLauncher/runtime/jre-x64/bin/java.exe
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto init
 
